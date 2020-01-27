1839973d3e9e431f956d8be9ab6d60cd4a9ead01
dabadi9 <43355819+dabadi9@users.noreply.github.com>
2020-01-27 18:37:51 -0500

    Update test_HW1.py
    Add more tests

--- test_HW1.py
+++ test_HW1.py
@@ -1,4 +1,19 @@
 import HW1
 
-def test_convertion():
+def test_convertions():
   assert HW1.arabic_to_roman(100) == "C"
+  assert HW1.arabic_to_roman(3999) == "MMMCMXCIX"
+  assert HW1.arabic_to_roman(893) == "DCCCXCIII"
+  assert HW1.arabic_to_roman(12) == "XII"
+  assert HW1.arabic_to_roman(1213) == "MCCXIII"
+  assert HW1.arabic_to_roman(432) == "CDXXXII"
+  assert HW1.arabic_to_roman(932) == "CMXXXII"
+  assert HW1.arabic_to_roman(1001) == "MI"
+  assert HW1.arabic_to_roman(384) == "CCCLXXXIV"
+
+ 
+def test_errors():
+  assert HW1.arabic_to_roman("100") == "ERROR"
+  assert HW1.arabic_to_roman(100.123) == "ERROR"
+  assert HW1.arabic_to_roman("test") == "ERROR"
+  assert HW1.arabic_to_roman(100.00) == "ERROR"
